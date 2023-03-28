54db435a4dc9e6f9603ca68e6ffaf0b6a51738cd
Ani <109935991+jarein1@users.noreply.github.com>
2023-03-28 22:48:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+//  Click Button Animation
+function openNav() {
+    document.getElementById("myNav").style.height = "80%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  }
+  
+  //Text Drop Animation
+  $(document).ready(function(){
+    $('#text-drop').mouseleave(function(){
+      $(this).removeClass('clicked');
+    }).click(function(){
+      $(this).addClass('clicked').html($(this).html());
+    });
+  });
